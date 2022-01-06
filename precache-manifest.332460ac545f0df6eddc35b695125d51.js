9d1eee4a55f9cf456f6c66fdd5d1611698dbbc6b
이종원 <201552@partner.sk.com>
2022-01-06 15:18:03 +0900

    Updates

---
+++ precache-manifest.332460ac545f0df6eddc35b695125d51.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "8419f99ac6b4d5bd6f68",
+    "url": "/react_test/static/css/main.e2bd66f1.chunk.css"
+  },
+  {
+    "revision": "8419f99ac6b4d5bd6f68",
+    "url": "/react_test/static/js/main.8419f99a.chunk.js"
+  },
+  {
+    "revision": "dbf16a7a1f5d12f53526",
+    "url": "/react_test/static/js/1.dbf16a7a.chunk.js"
+  },
+  {
+    "revision": "b75323fd636df612d81b",
+    "url": "/react_test/static/js/2.b75323fd.chunk.js"
+  },
+  {
+    "revision": "9a279e03d16869d37d8a",
+    "url": "/react_test/static/js/runtime~main.9a279e03.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/react_test/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "f6c7d43f5933d9cba4669e4c736a7437",
+    "url": "/react_test/index.html"
+  }
+];
